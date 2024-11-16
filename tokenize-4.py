f169141319d61b4b961e9b2287f71352547424c2
Aidan Do <aidand@canva.com>
2024-11-16 20:58:24 +1100

    .

--- tokenize-4.py
+++ tokenize-4.py
@@ -6,3 +6,10 @@ result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
 result = [item.strip() for item in result if item.strip()]
 
 print(result[:100])
+
+# Sort all tokens in the list
+all_words = sorted(set(result))
+print(len(all_words))
+
+# Token ids
+vocab = {word: i for i, word in enumerate(all_words)}
