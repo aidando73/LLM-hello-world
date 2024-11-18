e505f4cd6186fa21e4ca5d1f87ea7bd1d0d19900
Aidan Do <aidand@canva.com>
2024-11-18 20:16:57 +1100

    .

--- gpt.py
+++ gpt.py
@@ -1,5 +1,7 @@
 import torch
 from torch import nn
+from torch.utils.data import Dataset, DataLoader
+import tiktoken
 
 
 class GPTModel(nn.Module):
@@ -166,3 +168,38 @@ def generate_text_simple(model, idx,                 #1
         idx_next = torch.argmax(probas, dim=-1, keepdim=True)    #5
         idx = torch.cat((idx, idx_next), dim=1)     #6
     return idx
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)    #1
+
+        for i in range(0, len(token_ids) - max_length, stride):     #2
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):    #3
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):         #4
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+def create_dataloader_v1(txt, batch_size=4, max_length=256,
+                         stride=128, shuffle=True, drop_last=True,
+                         num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")                         #1
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)   #2
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,     #3
+        num_workers=num_workers     #4
+    )
+
+    return dataloader
