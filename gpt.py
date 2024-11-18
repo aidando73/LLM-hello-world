16d4c6530ba7e916dc57433d94118c00fc58a258
Aidan Do <aidand@canva.com>
2024-11-18 17:04:19 +1100

    .

--- gpt.py
+++ gpt.py
@@ -153,3 +153,16 @@ class MultiHeadAttention(nn.Module):
         )
         context_vec = self.out_proj(context_vec)    #11
         return context_vec
+
+def generate_text_simple(model, idx,                 #1
+                         max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]    #2
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]                    #3
+        probas = torch.softmax(logits, dim=-1)           #4
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)    #5
+        idx = torch.cat((idx, idx_next), dim=1)     #6
+    return idx
